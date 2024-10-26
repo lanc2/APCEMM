afec00b99c9cadff2e0e7d8a72c41954f47e4533
Lewis Clark <lanc2@cam.ac.uk>
2024-10-26 10:51:00 +0000

    HPC Example

---
+++ examples/Example4_HPC/Setup_HPC/compile_APCEMM.sh
@@ -0,0 +1,13 @@
+# Start in hpc-work
+module purge
+module load git-2.20.1-gcc-5.4.0-p7ladnf
+module load git/2.44.0
+git clone https://github.com/lanc2/APCEMM.git
+cd APCEMM
+git pull 
+git submodule update --init --recursive
+module purge
+module load cmake/latest
+mkdir build
+cd build
+cmake ../Code.v05-00/
