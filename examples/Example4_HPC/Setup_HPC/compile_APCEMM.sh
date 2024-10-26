da202a447608af9145773cc60e8b2dba6e010980
lanc2 <lanc2@cam.ac.uk>
2024-10-26 12:20:37 +0100

    Update compile_APCEMM.sh

--- examples/Example4_HPC/Setup_HPC/compile_APCEMM.sh
+++ examples/Example4_HPC/Setup_HPC/compile_APCEMM.sh
@@ -2,11 +2,12 @@
 module purge
 module load git-2.20.1-gcc-5.4.0-p7ladnf
 module load git/2.44.0
-git clone https://github.com/lanc2/APCEMM.git
+# git clone https://github.com/lanc2/APCEMM.git
 cd APCEMM
 git pull 
 git submodule update --init --recursive
 module purge
+module load gcc-7.2.0-gcc-4.8.5-pqn7o2k
 module load cmake/latest
 mkdir build
 cd build
